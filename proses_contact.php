631786fc19e7ef684e6152bbbbe66d066382f4b5
adrianpramana <adrian.astalina.pramana25@gmail.com>
2021-08-09 14:16:26 +0800

    initial commit

---
+++ proses_contact.php
@@ -0,0 +1,10 @@
+<?php 
+	$result = $_POST['name'];
+	echo "<strong>Result Name : </strong> <br/>"; echo "$result";
+	echo "<br/>";
+	$result = $_POST['email'];
+	echo "<strong>Result Email : </strong> <br/>"; echo "$result";
+	echo "<br/>";
+	$result = $_POST['message'];
+	echo "<strong>Result Message : </strong> <br/>"; echo "$result";
+ ?>
